ab050594e682a817fc74efcfe325f955abd59f5f
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-12 12:41:32 +0200

    Updated

--- proyectotechriders/src/app/services/service.email.ts
+++ proyectotechriders/src/app/services/service.email.ts
@@ -6,10 +6,9 @@ import { environment } from 'src/environments/environment.development';
 @Injectable()
 export class ServiceEmail {
   constructor(private _http: HttpClient) {}
-
+  private url = 'https://prod-20.westeurope.logic.azure.com:443/workflows/216f431a7f404a9590edb4bb69dcf216/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=UvB5yB-dXP-9tNyi9fxnODl3QyULN-Cl_95ZJtkCeeY';
+  
   sendEmail(asunto: string, cuerpo: string): Observable<any> {
-    let url = environment.urlApi;
-    let request = 'api/SendMail';
     let header = { 
       'Content-Type': 'application/json',
       Authorization: 'bearer ' + localStorage.getItem('token') 
@@ -19,6 +18,6 @@ export class ServiceEmail {
       'cuerpo': cuerpo
     });
 
-    return this._http.post(url + request, body, {headers: header});
+    return this._http.post(this.url, body, {headers: header});
   }
 }
