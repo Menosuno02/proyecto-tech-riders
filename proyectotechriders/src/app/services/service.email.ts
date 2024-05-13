cb5a24334113de43376451f1bcd1a554d1d61122
michelyp <michelypintom@gmail.com>
2024-05-13 14:17:26 +0200

    added new logic app

--- proyectotechriders/src/app/services/service.email.ts
+++ proyectotechriders/src/app/services/service.email.ts
@@ -6,8 +6,7 @@ import { environment } from 'src/environments/environment.development';
 @Injectable()
 export class ServiceEmail {
   constructor(private _http: HttpClient) {}
-  private url = 'https://prod-20.westeurope.logic.azure.com:443/workflows/216f431a7f404a9590edb4bb69dcf216/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=UvB5yB-dXP-9tNyi9fxnODl3QyULN-Cl_95ZJtkCeeY';
-
+  private url = environment.urlLogicContactoAdmin;
   sendEmail(asunto: string, cuerpo: string): Observable<any> {
     let header = { 
       'Content-Type': 'application/json'
--- proyectotechriders/src/environments/environment.development.ts
+++ proyectotechriders/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   urlApi: 'https://apitechriders.azurewebsites.net/',
+  urlLogicContactoAdmin: 'https://prod-236.westeurope.logic.azure.com:443/workflows/572d81c147e141dd80ddbf46775e7881/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=5_tYxgpF09IdfT1widAl8Rqni7aPm8RyqKu2rNCtbVY',
   idUsuario:0
 };
