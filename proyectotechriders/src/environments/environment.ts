a71051985a5725c295d54dfe163cfc6dc4549920
hmar321 <hmauricio.ar@gmail.com>
2024-05-13 13:31:16 +0200

    cambios mauricio

--- proyectotechriders/src/environments/environment.ts
+++ proyectotechriders/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   urlApi: 'https://apitechriders.azurewebsites.net/',
-  idUsuario:0
+  idUsuario:0,
+  urlLogicMailJhon: 'https://prod-67.westeurope.logic.azure.com:443/workflows/0d6dfa0674474e8ca7298dec8a956bff/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=iyp0J_p17H-wwWxAb-PPNykUGEW8sZViCx8Ij4Pn9XA'
 };
