0cf40e1b9d922df87e339fceef029cffcfd9dc54
Menosuno02 <83469483+Menosuno02@users.noreply.github.com>
2024-05-16 20:01:10 +0200

    Commit

--- proyectotechriders/src/app/components/recuperar-contrasena/recuperar-contrasena.component.ts
+++ proyectotechriders/src/app/components/recuperar-contrasena/recuperar-contrasena.component.ts
@@ -16,6 +16,7 @@ export class RecuperarContrasenaComponent implements OnInit {
 
   ngOnInit(): void {
     this.url = environment.domainUrl;
+    console.log(this.url);
   }
 
   recuperarcontrasenia(): void {
